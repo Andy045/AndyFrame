12217bf5a4c3feeb49516bce76fc7c3dc2dabdde
Liu Jie <liujie045.china@outlook.com>
2020-01-08 17:39:24 +0800

    Andy Commit

--- library/src/main/java/com/andy/frame/module/start/StartActivity.kt
+++ library/src/main/java/com/andy/frame/module/start/StartActivity.kt
@@ -1,10 +1,12 @@
 package com.andy.frame.module.start
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.annotation.DrawableRes
 import com.andy.basic.mvp.BasePresenter
 import com.andy.frame.R
 import com.andy.frame.base.FrameActivity
+import kotlinx.android.synthetic.main.hd_activity_start.*
 
 /**
  * @title: StartActivity
@@ -15,11 +17,36 @@ import com.andy.frame.base.FrameActivity
  */
 abstract class StartActivity : FrameActivity<BasePresenter>() {
 
+    init {
+        this.isCheckPermissions = true
+    }
+
+    var isFinishCurrent: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hd_activity_start)
+
+        val resId = setBackgroundImage()
+        if (resId != null) {
+            image.setImageResource(resId)
+        }
+    }
+
+    override fun onPermissionSuccessHDB() {
+        super.onPermissionSuccessHDB()
+
+        val intent = setIntentTargetActivity()
+        if (intent != null) {
+            startActivity(intent)
+            if (isFinishCurrent) {
+                finish()
+            }
+        }
     }
 
     @DrawableRes
-    abstract fun setBackgroundImage(): Int
+    abstract fun setBackgroundImage(): Int?
+
+    abstract fun setIntentTargetActivity(): Intent?
 }
