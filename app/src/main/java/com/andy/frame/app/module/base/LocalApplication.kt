096bf47c1f39db204f6f40d116420f9f9f150371
Liu Jie <liujie045.china@outlook.com>
2020-01-08 18:01:07 +0800

    Andy Commit

---
+++ app/src/main/java/com/andy/frame/app/module/base/LocalApplication.kt
@@ -0,0 +1,16 @@
+package com.andy.frame.app.module.base
+
+import com.andy.frame.base.FrameApplication
+
+/**
+ * @title: LocalApplication
+ * @projectName HandyFrame
+ * @description: TODO
+ * @author LiuJie https://github.com/Handy045
+ * @date Created in 2020-01-08 17:49
+ */
+class LocalApplication : FrameApplication() {
+    init {
+        this.buglyAppId = "d81893e792"
+    }
+}
---
+++ app/src/main/java/com/andy/frame/app/module/start/StartActivity.kt
@@ -0,0 +1,24 @@
+package com.andy.frame.app.module.start
+
+import android.content.Intent
+import com.andy.frame.app.R
+import com.andy.frame.app.module.main.MainActivity
+import com.andy.frame.module.start.StartActivity
+
+/**
+ * @title: StartActivity
+ * @projectName HandyFrame
+ * @description: TODO
+ * @author LiuJie https://github.com/Handy045
+ * @date Created in 2020-01-08 17:31
+ */
+class StartActivity : StartActivity() {
+
+    override fun setBackgroundImage(): Int? {
+        return R.drawable.hd_img_start_bg
+    }
+
+    override fun setIntentTargetActivity(): Intent? {
+        return Intent(this, MainActivity::class.java)
+    }
+}
