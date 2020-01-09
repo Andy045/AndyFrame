4816c5aafbfd6ee330f74cd71f8e72dcbadbeca1
Liu Jie <liujie045.china@outlook.com>
2020-01-09 17:54:44 +0800

    Andy Commit

--- library/src/main/java/com/andy/frame/module/start/StartActivity.kt
+++ library/src/main/java/com/andy/frame/module/start/StartActivity.kt
@@ -4,6 +4,7 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
 import android.provider.Settings
 import androidx.annotation.DrawableRes
 import com.andy.basic.mvp.BasePresenter
@@ -25,9 +26,29 @@ abstract class StartActivity : FrameActivity<BasePresenter>() {
         this.isCheckPermissions = true
     }
 
-    val permissionsRequestCode = 100
+    private val permissionsRequestCode = 100
 
-    var isFinishCurrent: Boolean = true
+    /**
+     * 跳转倒计时，默认1秒后执行跳转
+     */
+    var countTime = 1
+    private val mHandler: Handler = Handler()
+    private val countRunnable = object : Runnable {
+        override fun run() {
+            if (countTime > 0) {
+                mHandler.postDelayed(this, 1000)
+            } else if (countTime == 0) {
+                mHandler.removeCallbacks(this)
+
+                val intent = setIntentTargetActivity()
+                if (intent != null) {
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            countTime--
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,13 +64,7 @@ abstract class StartActivity : FrameActivity<BasePresenter>() {
     override fun onPermissionSuccessHD() {
         super.onPermissionSuccessHD()
 
-        val intent = setIntentTargetActivity()
-        if (intent != null) {
-            startActivity(intent)
-            if (isFinishCurrent) {
-                finish()
-            }
-        }
+        mHandler.postDelayed(countRunnable, 0)
     }
 
     override fun onPermissionRejectionHD(permissions: Array<String>) {
