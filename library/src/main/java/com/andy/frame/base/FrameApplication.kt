d117b0e567acb14a49780eaff3749d5f9d9e6e9a
Liu Jie <liujie045.china@outlook.com>
2020-01-09 09:39:57 +0800

    Andy Commit

--- library/src/main/java/com/andy/frame/base/FrameApplication.kt
+++ library/src/main/java/com/andy/frame/base/FrameApplication.kt
@@ -1,6 +1,8 @@
 package com.andy.frame.base
 
+import android.app.Application
 import com.andy.basic.base.BaseApplication
+import com.andy.frame.util.MaterialDialogUtil
 
 /**
  * @title: FrameApplication
@@ -9,4 +11,10 @@ import com.andy.basic.base.BaseApplication
  * @author LiuJie https://github.com/Handy045
  * @date Created in 2019-12-30 17:21
  */
-open class FrameApplication : BaseApplication()
+open class FrameApplication : BaseApplication() {
+
+    override fun onCreateHD(application: Application) {
+        super.onCreateHD(application)
+        MaterialDialogUtil.registerActivityLifecycle(this)
+    }
+}
