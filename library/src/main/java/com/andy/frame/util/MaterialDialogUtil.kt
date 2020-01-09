adac64ee936945697862fb30500524c223fd8e97
Liu Jie <liujie045.china@outlook.com>
2020-01-09 10:45:09 +0800

    Andy Commit

--- library/src/main/java/com/andy/frame/util/MaterialDialogUtil.kt
+++ library/src/main/java/com/andy/frame/util/MaterialDialogUtil.kt
@@ -20,6 +20,14 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
  */
 class MaterialDialogUtil {
 
+    enum class Type {
+        Normal,
+        Progress,
+        Success,
+        Error,
+        Warning
+    }
+
     companion object {
         var mDialog: AlertDialog? = null
         var mActivity: Activity? = null
@@ -67,6 +75,81 @@ class MaterialDialogUtil {
     }
 
     /**
+     * 通用提示框
+     *
+     * @param title 标题
+     * @param message 内容
+     * @param rBtnText 右侧按钮文本内容
+     * @param rBtnClickListener 右侧按钮点击事件
+     * @param lBtnText 左侧按钮文本内容
+     * @param lBtnClickListener 左侧按钮点击事件
+     * @param lastDismiss 如果上一个提示框还在呈现中，是否需要关闭
+     * @param backDismiss 通过返回键关闭提示框
+     * @param outsideDismiss 通过点击外部关闭提示框
+     */
+    fun showNormal(
+        title: String = "",
+        message: String = "",
+        rBtnText: String = "",
+        rBtnClickListener: DialogInterface.OnClickListener? = null,
+        lBtnText: String = "",
+        lBtnClickListener: DialogInterface.OnClickListener? = null,
+        cBtnText: String = "",
+        cBtnClickListener: DialogInterface.OnClickListener? = null,
+        lastDismiss: Boolean = true,
+        backDismiss: Boolean = true,
+        outsideDismiss: Boolean = true
+    ) {
+        if (mActivity != null && !mActivity!!.isFinishing) {
+            if (lastDismiss && mDialog != null && mDialog!!.isShowing) {
+                mDialog?.dismiss()
+            }
+
+            val builder = getBuilder(Type.Normal)
+            builder.setTitle(title)
+                .setMessage(message)
+                .setNeutralButton(lBtnText, lBtnClickListener)
+                .setNegativeButton(cBtnText, cBtnClickListener)
+                .setPositiveButton(rBtnText, rBtnClickListener)
+            mDialog = builder.show()
+            mDialog?.window?.findViewById<Space>(R.id.spacer)?.visibility = View.GONE
+            mDialog?.window?.findViewById<FrameLayout>(R.id.contentPanel)?.minimumHeight = 0
+            mDialog?.setCancelable(backDismiss)
+            mDialog?.setCanceledOnTouchOutside(outsideDismiss)
+        }
+    }
+
+    /**
+     * 进度提示框
+     *
+     * @param message 提示内容
+     * @param lastDismiss 如果上一个提示框还在呈现中，是否需要关闭
+     * @param backDismiss 通过返回键关闭提示框
+     * @param outsideDismiss 通过点击外部关闭提示框
+     */
+    fun showProgress(
+        title: String = "",
+        lastDismiss: Boolean = true,
+        backDismiss: Boolean = false,
+        outsideDismiss: Boolean = false
+    ) {
+        if (mActivity != null && !mActivity!!.isFinishing) {
+            if (lastDismiss && mDialog != null && mDialog!!.isShowing) {
+                mDialog?.dismiss()
+            }
+
+            val builder = getBuilder(Type.Progress)
+            builder.setIcon(R.drawable.hd_anim_materialdialog_progress)
+            builder.setMessage(title)
+            mDialog = builder.show()
+            mDialog?.window?.findViewById<Space>(R.id.spacer)?.visibility = View.GONE
+            mDialog?.window?.findViewById<FrameLayout>(R.id.contentPanel)?.minimumHeight = 0
+            mDialog?.setCancelable(backDismiss)
+            mDialog?.setCanceledOnTouchOutside(outsideDismiss)
+        }
+    }
+
+    /**
      * 成功提示框
      *
      * @param title 标题
@@ -91,11 +174,13 @@ class MaterialDialogUtil {
                 mDialog?.dismiss()
             }
 
-            val builder = getBuilder()
+            val builder = getBuilder(Type.Success)
             builder.setTitle(title)
                 .setMessage(message)
                 .setNegativeButton(cBtnText, cBtnClickListener)
             mDialog = builder.show()
+            mDialog?.window?.findViewById<Space>(R.id.spacer)?.visibility = View.GONE
+            mDialog?.window?.findViewById<FrameLayout>(R.id.contentPanel)?.minimumHeight = 0
             mDialog?.setCancelable(backDismiss)
             mDialog?.setCanceledOnTouchOutside(outsideDismiss)
         }
@@ -126,11 +211,13 @@ class MaterialDialogUtil {
                 mDialog?.dismiss()
             }
 
-            val builder = getBuilder()
+            val builder = getBuilder(Type.Error)
             builder.setTitle(title)
                 .setMessage(message)
                 .setNegativeButton(cBtnText, cBtnClickListener)
             mDialog = builder.show()
+            mDialog?.window?.findViewById<Space>(R.id.spacer)?.visibility = View.GONE
+            mDialog?.window?.findViewById<FrameLayout>(R.id.contentPanel)?.minimumHeight = 0
             mDialog?.setCancelable(backDismiss)
             mDialog?.setCanceledOnTouchOutside(outsideDismiss)
         }
@@ -161,81 +248,13 @@ class MaterialDialogUtil {
                 mDialog?.dismiss()
             }
 
-            val builder = getBuilder()
+            val builder = getBuilder(Type.Warning)
             builder.setTitle(title)
                 .setMessage(message)
                 .setNegativeButton(cBtnText, cBtnClickListener)
             mDialog = builder.show()
-            mDialog?.setCancelable(backDismiss)
-            mDialog?.setCanceledOnTouchOutside(outsideDismiss)
-        }
-    }
-
-    /**
-     * 通用提示框
-     *
-     * @param title 标题
-     * @param message 内容
-     * @param rBtnText 右侧按钮文本内容
-     * @param rBtnClickListener 右侧按钮点击事件
-     * @param lBtnText 左侧按钮文本内容
-     * @param lBtnClickListener 左侧按钮点击事件
-     * @param lastDismiss 如果上一个提示框还在呈现中，是否需要关闭
-     * @param backDismiss 通过返回键关闭提示框
-     * @param outsideDismiss 通过点击外部关闭提示框
-     */
-    fun showNormal(
-        title: String = "",
-        message: String = "",
-        rBtnText: String = "",
-        rBtnClickListener: DialogInterface.OnClickListener? = null,
-        lBtnText: String = "",
-        lBtnClickListener: DialogInterface.OnClickListener? = null,
-        lastDismiss: Boolean = true,
-        backDismiss: Boolean = true,
-        outsideDismiss: Boolean = true
-    ) {
-        if (mActivity != null && !mActivity!!.isFinishing) {
-            if (lastDismiss && mDialog != null && mDialog!!.isShowing) {
-                mDialog?.dismiss()
-            }
-
-            val builder = getBuilder()
-            builder.setTitle(title)
-                .setMessage(message)
-                .setNeutralButton(lBtnText, lBtnClickListener)
-                .setNegativeButton(lBtnText, lBtnClickListener)
-                .setPositiveButton(rBtnText, rBtnClickListener)
-            mDialog = builder.show()
             mDialog?.window?.findViewById<Space>(R.id.spacer)?.visibility = View.GONE
             mDialog?.window?.findViewById<FrameLayout>(R.id.contentPanel)?.minimumHeight = 0
-            mDialog?.setCancelable(backDismiss)
-            mDialog?.setCanceledOnTouchOutside(outsideDismiss)
-        }
-    }
-
-    /**
-     * 进度提示框
-     *
-     * @param message 提示内容
-     * @param lastDismiss 如果上一个提示框还在呈现中，是否需要关闭
-     * @param backDismiss 通过返回键关闭提示框
-     * @param outsideDismiss 通过点击外部关闭提示框
-     */
-    fun showProgress(
-        title: String = "",
-        lastDismiss: Boolean = true,
-        backDismiss: Boolean = false,
-        outsideDismiss: Boolean = false
-    ) {
-        if (mActivity != null && !mActivity!!.isFinishing) {
-            if (lastDismiss && mDialog != null && mDialog!!.isShowing) {
-                mDialog?.dismiss()
-            }
-
-            val builder = getBuilder()
-            builder.setMessage(title)
-            mDialog = builder.show()
             mDialog?.setCancelable(backDismiss)
             mDialog?.setCanceledOnTouchOutside(outsideDismiss)
         }
@@ -247,7 +266,23 @@ class MaterialDialogUtil {
         }
     }
 
-    fun getBuilder(): MaterialAlertDialogBuilder {
-        return MaterialAlertDialogBuilder(mActivity, R.style.AD_MaterialDialogTheme)
+    fun getBuilder(type: Type): MaterialAlertDialogBuilder {
+        return when (type) {
+            Type.Normal -> {
+                MaterialAlertDialogBuilder(mActivity, R.style.AD_MaterialDialogTheme_Normal)
+            }
+            Type.Progress -> {
+                MaterialAlertDialogBuilder(mActivity, R.style.AD_MaterialDialogTheme)
+            }
+            Type.Success -> {
+                MaterialAlertDialogBuilder(mActivity, R.style.AD_MaterialDialogTheme_Success)
+            }
+            Type.Error -> {
+                MaterialAlertDialogBuilder(mActivity, R.style.AD_MaterialDialogTheme_Error)
+            }
+            Type.Warning -> {
+                MaterialAlertDialogBuilder(mActivity, R.style.AD_MaterialDialogTheme_Warning)
+            }
+        }
     }
 }
