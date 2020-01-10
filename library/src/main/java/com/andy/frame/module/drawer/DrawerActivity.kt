b714b757653441f020c4bd8291914ca8d057fa66
Liu Jie <liujie045.china@outlook.com>
2020-01-10 11:05:31 +0800

    Andy Commit

--- library/src/main/java/com/andy/frame/module/drawer/DrawerActivity.kt
+++ library/src/main/java/com/andy/frame/module/drawer/DrawerActivity.kt
@@ -2,6 +2,7 @@ package com.andy.frame.module.drawer
 
 import android.os.Bundle
 import android.view.View
+import android.widget.FrameLayout
 import androidx.drawerlayout.widget.DrawerLayout
 import com.andy.basic.mvp.BasePresenter
 import com.andy.frame.R
@@ -16,40 +17,84 @@ import kotlinx.android.synthetic.main.hd_activity_drawer.*
  * @date Created in 2020-01-10 08:50
  */
 open class DrawerActivity<P : BasePresenter> : FrameActivity<P>() {
+
+    var mDrawerLeft: View? = null
+    var mDrawerRight: View? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.hd_activity_drawer)
 
-        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, drawerChildLeft)
-        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, drawerChildRight)
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, drawerLeft)
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, drawerRight)
+    }
+
+    //==================================================
+    // Content
+    //==================================================
+    fun getFrameLayoutCenter(): FrameLayout {
+        return frameLayoutCenter
     }
 
     fun setCenterFrameLayout(view: View) {
         frameLayoutCenter.addView(view)
     }
 
-    fun setLeftDrawerFrameLayout(view: View) {
+    //==================================================
+    // DrawerLeft
+    //==================================================
+    fun openDrawerLeft() {
+        if (drawerLeft != null) {
+            drawerLayout.openDrawer(drawerLeft!!)
+        }
+    }
+
+    fun closeDrawerLeft() {
+        if (drawerLeft != null) {
+            drawerLayout.closeDrawer(drawerLeft!!)
+        }
+    }
+
+    fun setFrameLayoutLeftView(view: View) {
+        mDrawerLeft = view
+        drawerLeft.visibility = View.VISIBLE
         frameLayoutLeft.addView(view)
-        drawerChildLeft.visibility = View.VISIBLE
-        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED, drawerChildLeft)
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED, drawerLeft)
     }
 
-    fun setRightDrawerFrameLayout(view: View) {
-        frameLayoutRight.addView(view)
-        drawerChildRight.visibility = View.VISIBLE
-        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED, drawerChildRight)
-    }
-
-    fun removeLeftDrawerFrameLayoutViews() {
-        drawerChildLeft.visibility = View.GONE
+    fun removeFrameLayoutLeftView() {
+        mDrawerLeft = null
+        drawerLeft.visibility = View.GONE
         frameLayoutLeft.removeAllViews()
-        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, drawerChildLeft)
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, drawerLeft)
     }
 
-    fun removeRightDrawerFrameLayoutViews() {
-        drawerChildRight.visibility = View.GONE
+    //==================================================
+    // DrawerRight
+    //==================================================
+    fun openDrawerRight() {
+        if (drawerRight != null) {
+            drawerLayout.openDrawer(drawerRight!!)
+        }
+    }
+
+    fun closeDrawerRight() {
+        if (drawerRight != null) {
+            drawerLayout.closeDrawer(drawerRight!!)
+        }
+    }
+
+    fun setFrameLayoutRightView(view: View) {
+        mDrawerRight = view
+        drawerRight.visibility = View.VISIBLE
+        frameLayoutRight.addView(view)
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED, drawerRight)
+    }
+
+    fun removeFrameLayoutRightView() {
+        mDrawerRight = null
+        drawerRight.visibility = View.GONE
         frameLayoutRight.removeAllViews()
-        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, drawerChildRight)
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, drawerRight)
     }
 }
