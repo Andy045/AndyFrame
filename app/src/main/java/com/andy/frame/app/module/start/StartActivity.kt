60876d568ffe7ac889bf80ae1ec519385911d0c9
Liu Jie <liujie045.china@outlook.com>
2020-01-10 09:00:42 +0800

    Andy Commit

--- app/src/main/java/com/andy/frame/app/module/start/StartActivity.kt
+++ app/src/main/java/com/andy/frame/app/module/start/StartActivity.kt
@@ -1,6 +1,7 @@
 package com.andy.frame.app.module.start
 
 import android.content.Intent
+import com.andy.basic.mvp.BasePresenter
 import com.andy.frame.app.R
 import com.andy.frame.app.module.main.MainActivity
 import com.andy.frame.module.start.StartActivity
@@ -12,7 +13,7 @@ import com.andy.frame.module.start.StartActivity
  * @author LiuJie https://github.com/Handy045
  * @date Created in 2020-01-08 17:31
  */
-class StartActivity : StartActivity() {
+class StartActivity : StartActivity<BasePresenter>() {
 
     override fun setBackgroundImage(): Int? {
         return R.drawable.hd_img_start_bg
