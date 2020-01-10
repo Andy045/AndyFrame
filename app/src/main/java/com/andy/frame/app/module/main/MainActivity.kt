08a57d5a291b6974c86fd46fe12903580a9a968b
Liu Jie <liujie045.china@outlook.com>
2020-01-10 08:38:15 +0800

    Andy Commit

--- app/src/main/java/com/andy/frame/app/module/main/MainActivity.kt
+++ app/src/main/java/com/andy/frame/app/module/main/MainActivity.kt
@@ -1,9 +1,17 @@
-package com.andy.frame.app
+package com.andy.frame.app.module.main
 
 import android.os.Bundle
 import com.andy.basic.mvp.BasePresenter
+import com.andy.frame.app.R
 import com.andy.frame.base.FrameActivity
 
+/**
+ * @title: StartActivity
+ * @projectName HandyFrame
+ * @description: 首页界面
+ * @author LiuJie https://github.com/Handy045
+ * @date Created in 2020-01-08 17:31
+ */
 class MainActivity : FrameActivity<BasePresenter>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
