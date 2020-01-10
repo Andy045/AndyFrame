7dd59d456122e0a81dab0a704bf655204d2c5f07
Liu Jie <liujie045.china@outlook.com>
2020-01-10 17:37:13 +0800

    Andy Commit

--- app/src/main/java/com/andy/frame/app/module/main/MainActivity.kt
+++ app/src/main/java/com/andy/frame/app/module/main/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.andy.frame.app.module.main
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
 import com.andy.basic.mvp.BasePresenter
 import com.andy.frame.app.R
 import com.andy.frame.module.drawer.DrawerActivity
+import kotlinx.android.synthetic.main.hd_include_main_drawer_right.*
 
 /**
  * @title: StartActivity
@@ -17,14 +19,28 @@ class MainActivity : DrawerActivity<BasePresenter>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         getTitlebar().setTitleText("首页")
     }
 
     override fun initViewHD(savedInstanceState: Bundle?) {
         super.initViewHD(savedInstanceState)
 
-        val view =
-            LayoutInflater.from(mContext).inflate(R.layout.hd_include_main_drawer_right, null)
-        addDrawerLeftView(view)
+        addDrawerLeftView(R.layout.hd_include_main_drawer_right)
+
+        tvGithub.setOnClickListener {
+            val uri = Uri.parse("https://github.com/Andy045")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+        tvWebSite.setOnClickListener {
+            val uri = Uri.parse("https://handy045.com")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+        tvSetting.setOnClickListener { }
+        tvExitApp.setOnClickListener { finish() }
     }
 }
